368a1a527a2fdfafb62dceb45c601ada7514b7f7
junichi-fujita <fujita0909j@gmail.com>
2020-02-19 21:23:59 +0900

    controller usersの作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    
+  end
+end
