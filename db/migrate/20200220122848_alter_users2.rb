624713fa2169228955ca1885a35fa39e4a773607
junichi-fujita <fujita0909j@gmail.com>
2020-02-20 21:33:46 +0900

    modelにemailカラムを追加

---
+++ db/migrate/20200220122848_alter_users2.rb
@@ -0,0 +1,5 @@
+class AlterUsers2 < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :email, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 20.times do |n|
   name  = "example-#{n+1}"
+  email = "example-#{n+1}@sample.com"
   User.create!(
     name:  name,
+    email: email,
     password: "aaa",
     password_confirmation: "aaa",
   )
