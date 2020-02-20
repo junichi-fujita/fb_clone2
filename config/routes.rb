b73396695c7078778e5a04b37c1519fe3f9888a5
junichi-fujita <fujita0909j@gmail.com>
2020-02-20 23:28:03 +0900

    アカウントのshow画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'accounts/show'
+  get 'accounts/edit'
   root "top#home"
   resources :users
   resource :session, only: [:create, :destroy]
+  resource :account, only: [:show, :edit, :update]
 end
