03871e39fb998eee784cdf33e7b13d7a20c89f5f
junichi-fujita <fujita0909j@gmail.com>
2020-02-21 21:06:13 +0900

    アカウントの編集機能を作成

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -6,5 +6,23 @@ class AccountsController < ApplicationController
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.assign_attributes(account_params)
+    if @user.save
+      redirect_to :account, notice: "アカウント情報を更新しました。"
+    else
+      render "edit"
+    end
+  end
+
+  private def account_params
+    params[:account].permit(
+      :name,
+      :email,
+    )
   end
 end
