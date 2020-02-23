ef7bee8b011dd160bae05072002697f536d0c6b7
junichi-fujita <fujita0909j@gmail.com>
2020-02-23 18:56:47 +0900

    エラー画面の設定

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -1,6 +1,6 @@
 module PicturesHelper
   def choose_new_or_edit
-    if action_name == "new"
+    if action_name == "new" || action_name == ""
       :confirm_pictures
     elsif action_name == "edit"
       :picture
